63630876dfa8ba2165d2a73265f8bcc41144ce88
chao2 <380692952@qq.com>
2014-01-08 10:26:51 +0800

    bugfix.1.远程模块没有被加进依赖数组；2.server的onRequest判断逻辑有问题

---
+++ test/project6/resource/dist/script/page/main.js
@@ -0,0 +1,15 @@
+/*! lastmodify: 2014-01-08 09:49:37 */
+define("module/a",[],function(require, exports, module){
+	console.log('module a');
+	module.exports = 'module a';
+});;
+define("page/main_widgets/_top",[],function(require){
+	console.log('main - top');
+	return '_top';
+});;
+define("page/main",["module/a","http://webresource.c-ctrip.com/code/cquery/mod/calendar-6.0.js","page/main_widgets/_top"],function(require){
+	require('../module/a');
+	require('http://webresource.c-ctrip.com/code/cquery/mod/calendar-6.0.js');
+	require('./main_widgets/_top');
+	console.log('page main');
+});;
